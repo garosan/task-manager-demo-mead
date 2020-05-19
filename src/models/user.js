00885e9b38158edb3c0f88a8c77dd130c112a995
Garo San <garosanchez27@gmail.com>
2020-05-19 00:40:05 -0500

    Refactor models and other changes

---
+++ src/models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+  },
+  age: {
+    type: Number,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid.");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    validate(value) {
+      if (value.length < 6) {
+        throw new Error("Password must be at least 6 characters long.");
+      }
+
+      if (value.toLowerCase().includes("password")) {
+        throw new Error("Password cannot include the word 'password'.");
+      }
+    },
+  },
+});
+
+module.exports = User;
