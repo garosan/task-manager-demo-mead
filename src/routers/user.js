63c14d4bf325587e2399f6dce0e74b3e00e248a8
Garo San <garosanchez27@gmail.com>
2020-05-22 20:31:06 -0500

    Add more middleware and routes

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const User = require("../models/user");
+const auth = require("../middleware/auth");
 const router = new express.Router();
 
 router.post("/users", async (req, res) => {
@@ -24,27 +25,27 @@ router.post("/users/login", async (req, res) => {
   }
 });
 
-router.get("/users", async (req, res) => {
+router.post("/users/logout", auth, async (req, res) => {
   try {
-    const users = await User.find({});
-    res.send(users);
+    req.user.tokens = req.user.tokens.filter(() => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
+
+    res.send();
   } catch (e) {
-    res.status(500).send(e);
+    res.status(500).send();
   }
 });
 
-router.get("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-
+router.post("/users/logoutAll", auth, async (req, res) => {
   try {
-    const user = await User.findById(_id);
-    if (!user) {
-      return res.status(404).send();
-    } else {
-      res.send(user);
-    }
+    req.user.tokens = [];
+    await req.user.save();
+
+    res.send();
   } catch (e) {
-    res.status(500).send(e);
+    res.status(500).send();
   }
 });
 
